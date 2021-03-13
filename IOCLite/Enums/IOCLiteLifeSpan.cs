fb2cc7930f85bdd454de91f59b5a6d0e77a079f7
Kieron Drumm <klrdrumm@outlook.com>
2021-03-13 21:58:13 +0000

    Added more XML documentation.

--- IOCLite/Enums/IOCLiteLifeSpan.cs
+++ IOCLite/Enums/IOCLiteLifeSpan.cs
@@ -5,7 +5,16 @@
     /// </summary>
     public enum IOCLiteLifeSpan
     {
+        /// <summary>
+        /// Specifies that a single instance of the registration should be created for
+        /// the duration of the application's lifespan. 
+        /// </summary>
         SINGLETON,
+        
+        /// <summary>
+        /// Specifies that a new instance should be created everyting a Resolve() call
+        /// is made to the IOCLite container.
+        /// </summary>
         TRANSIENT
     }
 }
